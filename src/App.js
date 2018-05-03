0adf5d9bdaaacdc0d559d2cba6eb8abcfbd2df9b
Mark <mark.sainty@gmail.com>
2018-05-03 10:24:29 +0100

    removed image and update text on welcome page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,11 +6,10 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to First Half Goals</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          This page will list opportunities to bet on a goal being scored in the first half.  The algorithm will try to find games where this is likely to happen.
         </p>
       </div>
     );
